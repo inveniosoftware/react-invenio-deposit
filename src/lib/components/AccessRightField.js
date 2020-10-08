8cb590fb23185f537517c634f1866c8e91b93309
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2020-10-08 14:28:06 +0200

    access: fix initial checked value

--- src/lib/components/AccessRightField.js
+++ src/lib/components/AccessRightField.js
@@ -6,22 +6,19 @@
 // under the terms of the MIT License; see LICENSE file for more details.
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import _get from 'lodash/get';
 
-import { ErrorLabel, FieldLabel, RadioField, GroupField } from 'react-invenio-forms';
+import {
+  ErrorLabel,
+  FieldLabel,
+  RadioField,
+  GroupField,
+} from 'react-invenio-forms';
 import { Card, Form } from 'semantic-ui-react';
+import { Field } from 'formik';
 
 export class AccessRightField extends Component {
   /** Top-level Access Right Component */
-  constructor(props) {
-    super(props);
-    this.state = {
-      selected: null,
-    };
-  }
-
-  handleChange = (e, { value }) => {
-    this.setState({ selected: value });
-  };
 
   render() {
     const { fieldPath, label, labelIcon, options } = this.props;
@@ -33,15 +30,21 @@ export class AccessRightField extends Component {
             <Form.Field required>
               <FieldLabel htmlFor={fieldPath} icon={labelIcon} label={label} />
               {options.map((option) => (
-                <RadioField
-                  fieldPath={fieldPath}
-                  label={option.text}
-                  labelIcon={option.icon}
-                  key={option.value}
-                  value={option.value}
-                  checked={this.state.selected === option.value}
-                  onChange={this.handleChange}
-                />
+                <Field>
+                  {({ field }) => {
+                    return (
+                      <RadioField
+                        fieldPath={fieldPath}
+                        label={option.text}
+                        labelIcon={option.icon}
+                        key={option.value}
+                        value={option.value}
+                        checked={_get(field.value, fieldPath) === option.value}
+                        onChange={() => {}}
+                      />
+                    );
+                  }}
+                </Field>
               ))}
               <ErrorLabel fieldPath={fieldPath} />
             </Form.Field>
