019cd42c2c84ea52aa13312a73d88dd9b40fd92e
Zacharias Zacharodimos <zacharias.zacharodimos@cern.ch>
2021-02-08 17:27:52 +0100

    license: add drag-n-drop ordering of licenses

--- src/lib/components/License/LicenseField.js
+++ src/lib/components/License/LicenseField.js
@@ -1,6 +1,6 @@
 // This file is part of React-Invenio-Deposit
-// Copyright (C) 2020 CERN.
-// Copyright (C) 2020 Northwestern University.
+// Copyright (C) 2020-2021 CERN.
+// Copyright (C) 2020-2021 Northwestern University.
 //
 // React-Invenio-Deposit is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
@@ -8,109 +8,107 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { getIn, FieldArray } from 'formik';
-
-import { Button, Form, List, Icon } from 'semantic-ui-react';
-
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { DndProvider } from 'react-dnd';
 import { FieldLabel } from 'react-invenio-forms';
+import { Button, Form, Icon, List } from 'semantic-ui-react';
+
 import { LicenseModal } from './LicenseModal';
+import { LicenseFieldItem } from './LicenseFieldItem';
+
+class LicenseFieldForm extends Component {
+  render() {
+    const {
+      label,
+      labelIcon,
+      fieldPath,
+      form: { values, errors },
+      move: formikArrayMove,
+      push: formikArrayPush,
+      remove: formikArrayRemove,
+      replace: formikArrayReplace,
+      required,
+    } = this.props;
+    return (
+      <DndProvider backend={HTML5Backend}>
+        <Form.Field required={required}>
+          <FieldLabel
+            htmlFor={fieldPath}
+            icon={labelIcon}
+            label={label}
+          ></FieldLabel>
+          <List>
+            {getIn(values, fieldPath, []).map((value, index, array) => {
+              const arrayPath = fieldPath;
+              const indexPath = index;
+              const key = `${arrayPath}.${indexPath}`;
+              const licenseType = value.id ? 'standard' : 'custom';
+              const description = getIn(values, `${key}.description`, '');
+              const title = getIn(values, `${key}.title`, '');
+              return (
+                <LicenseFieldItem
+                  key={key}
+                  {...{
+                    index,
+                    licenseType,
+                    compKey: key,
+                    initialLicense: licenseType === 'custom' ? value : null,
+                    licenseDescription: description,
+                    licenseTitle: title,
+                    moveLicense: formikArrayMove,
+                    replaceLicense: formikArrayReplace,
+                    removeLicense: formikArrayRemove,
+                    searchConfig: this.props.searchConfig,
+                    serializeLicenses: this.props.serializeLicenses,
+                  }}
+                />
+              );
+            })}
+            <LicenseModal
+              searchConfig={this.props.searchConfig}
+              trigger={
+                <Button type="button" key="standard">
+                  <Icon name="add" />
+                  Add standard
+                </Button>
+              }
+              onLicenseChange={(selectedLicense) => {
+                formikArrayPush(selectedLicense);
+              }}
+              mode="standard"
+              action="add"
+              serializeLicenses={this.props.serializeLicenses}
+            />
+            <LicenseModal
+              searchConfig={this.props.searchConfig}
+              trigger={
+                <Button type="button" key="custom">
+                  <Icon name="add" />
+                  Add custom
+                </Button>
+              }
+              onLicenseChange={(selectedLicense) => {
+                formikArrayPush(selectedLicense);
+              }}
+              mode="custom"
+              action="add"
+            />
+          </List>
+        </Form.Field>
+      </DndProvider>
+    );
+  }
+  setOpen = (open) => this.setState({ open });
+}
 
 export class LicenseField extends Component {
-  renderFormField = (props) => {
-    const {
-      form: { values, errors },
-      ...arrayHelpers
-    } = props;
-    const { fieldPath, label, labelIcon, required } = this.props;
-    return (
-      <Form.Field required={required}>
-        <FieldLabel
-          htmlFor={fieldPath}
-          icon={labelIcon}
-          label={label}
-        ></FieldLabel>
-        <List>
-          {getIn(values, fieldPath, []).map((value, index, array) => {
-            const arrayPath = fieldPath;
-            const indexPath = index;
-            const key = `${arrayPath}.${indexPath}`;
-            const licenseType = value.id ? 'standard' : 'custom';
-            const description = getIn(values, `${key}.description`, '');
-            const title = getIn(values, `${key}.title`, '');
-            return (
-              <List.Item key={key} className="license-listitem">
-                <List.Content floated="right">
-                  <LicenseModal
-                    searchConfig={this.props.searchConfig}
-                    onLicenseChange={(selectedLicense) => {
-                      arrayHelpers.replace(indexPath, selectedLicense);
-                    }}
-                    mode={licenseType}
-                    initialLicense={licenseType === 'custom' ? value : null}
-                    action="edit"
-                    trigger={
-                      <Button size="mini" primary type="button">
-                        Change
-                      </Button>
-                    }
-                    serializeLicenses={this.props.serializeLicenses}
-                  />
-                  <Button
-                    size="mini"
-                    type="button"
-                    onClick={() => arrayHelpers.remove(indexPath)}
-                  >
-                    Remove
-                  </Button>
-                </List.Content>
-                <List.Content>
-                  <List.Header>{title}</List.Header>
-                  {description && (
-                    <List.Description>{description}</List.Description>
-                  )}
-                </List.Content>
-              </List.Item>
-            );
-          })}
-          <LicenseModal
-            searchConfig={this.props.searchConfig}
-            trigger={
-              <Button type="button" key="standard" className="add-licenses">
-                <Icon name="add" />
-                Add standard
-              </Button>
-            }
-            onLicenseChange={(selectedLicense) => {
-              arrayHelpers.push(selectedLicense);
-            }}
-            mode="standard"
-            action="add"
-            serializeLicenses={this.props.serializeLicenses}
-          />
-          <LicenseModal
-            searchConfig={this.props.searchConfig}
-            trigger={
-              <Button type="button" key="custom" className="add-licenses">
-                <Icon name="add" />
-                Add custom
-              </Button>
-            }
-            onLicenseChange={(selectedLicense) => {
-              arrayHelpers.push(selectedLicense);
-            }}
-            mode="custom"
-            action="add"
-          />
-        </List>
-      </Form.Field>
-    );
-  };
-  setOpen = (open) => this.setState({ open });
-
   render() {
     return (
       <FieldArray
         name={this.props.fieldPath}
-        component={this.renderFormField}
+        component={(formikProps) => (
+          <LicenseFieldForm {...formikProps} {...this.props} />
+        )}
       />
     );
   }
