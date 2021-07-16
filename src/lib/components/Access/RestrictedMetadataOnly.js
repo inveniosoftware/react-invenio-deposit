bef3df0aa81cf1c175742628ec098c4174970561
mb-wali <mb_wali@hotmail.com>
2021-07-16 19:06:52 +0200

    i18n: adds german translations

--- src/lib/components/Access/RestrictedMetadataOnly.js
+++ src/lib/components/Access/RestrictedMetadataOnly.js
@@ -1,11 +1,13 @@
 // This file is part of React-Invenio-Deposit
 // Copyright (C) 2020-2021 CERN.
 // Copyright (C) 2020-2021 Northwestern University.
+// Copyright (C) 2021 Graz University of Technology.
 //
 // React-Invenio-Deposit is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
 import React from 'react';
-
+import { i18next } from '@translations/i18next';
+import { Trans } from 'react-i18next';
 import { embargoSection, filesSection, MessageSection, MetadataSection } from './utils';
 
 // Restricted no files
@@ -25,17 +27,22 @@ export class RestrictedMetadataOnly {
       opacity: "0.5",
       cursor: "default !important"
     };
-    let filesContent = <p style={{...filesStyle, textAlign: "center"}}><em>The record has no files.</em></p>;
+    let filesContent = <p style={{...filesStyle, textAlign: "center"}}><em>{i18next.t('The record has no files.')}</em></p>;
     return filesSection(filesStyle, filesContent);
   }
 
   renderMessageSection() {
-    const text = <>The record can <b>only</b> be accessed by <b>users specified</b> in the permissions.</>;
+    const text = (
+      <Trans>
+        The record can <b>only</b> be accessed by <b>users specified</b> in the
+        permissions.
+      </Trans>
+    ); 
 
     return <MessageSection
       intent={{negative: true}}
       icon="lock"
-      title="Restricted"
+      title={i18next.t('Restricted')}
       text={text}
     />;
 
