b0102c6c479b47d0beecec763a8f64f6f2443e73
mb-wali <mb_wali@hotmail.com>
2021-07-16 16:04:00 +0200

    i18n: mark/extract missing strings

--- src/lib/components/Access/PublicMetadataOnly.js
+++ src/lib/components/Access/PublicMetadataOnly.js
@@ -1,12 +1,14 @@
 // This file is part of React-Invenio-Deposit
 // Copyright (C) 2020-2021 CERN.
 // Copyright (C) 2020-2021 Northwestern University.
+// Copyright (C) 2021 Graz University of Technology.
 //
 // React-Invenio-Deposit is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
 import React from 'react';
 
 import { embargoSection, filesSection, MessageSection, MetadataSection } from './utils';
+import { i18next } from '@translations/i18next';
 
 // Public record no files
 export class PublicMetadataOnly {
@@ -23,18 +25,18 @@ export class PublicMetadataOnly {
       opacity: "0.5",
       cursor: "default !important"
     };
-    let filesContent = <p style={{...filesStyle, textAlign: "center"}}><em>The record has no files.</em></p>;
+    let filesContent = <p style={{...filesStyle, textAlign: "center"}}><em>{i18next.t('The record has no files.')}</em></p>;
 
     return filesSection(filesStyle, filesContent);
   }
 
   renderMessageSection() {
-    const text = "The record is publicly accessible.";
+    const text = i18next.t('The record is publicly accessible.');
 
     return <MessageSection
       intent={{positive: true}}
       icon="lock open"
-      title="Public"
+      title={i18next.t('Public')}
       text={text}
     />;
   }
