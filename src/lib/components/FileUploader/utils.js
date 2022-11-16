9568fc363870087f797f60c3d7c657bc68169ba3
Manuel Alejandro <mumox7@gmail.com>
2022-11-16 10:35:29 +0100

    utils: refactored function to return a string instead of jsx.

--- src/lib/components/FileUploader/utils.js
+++ src/lib/components/FileUploader/utils.js
@@ -6,8 +6,6 @@
 // React-Invenio-Deposit is free software; you can redistribute it and/or modify it
 // under the terms of the MIT License; see LICENSE file for more details.
 
-import React from "react";
-
 import _isNumber from "lodash/isNumber";
 
 export function humanReadableBytes(bytes, decimalDisplay = false) {
@@ -18,25 +16,13 @@ export function humanReadableBytes(bytes, decimalDisplay = false) {
     const gigaBytes = base * megaBytes;
 
     if (bytes < kiloBytes) {
-      return <>{bytes} bytes</>;
+      return `${bytes} bytes`;
     } else if (bytes < megaBytes) {
-      return (
-        <>
-          {(bytes / kiloBytes).toFixed(2)} {decimalDisplay ? "KB" : "KiB"}
-        </>
-      );
+      return `${(bytes / kiloBytes).toFixed(2)} ${decimalDisplay ? "KB" : "KiB"}`;
     } else if (bytes < gigaBytes) {
-      return (
-        <>
-          {(bytes / megaBytes).toFixed(2)} {decimalDisplay ? "MB" : "MiB"}
-        </>
-      );
+      return `${(bytes / megaBytes).toFixed(2)} ${decimalDisplay ? "MB" : "MiB"}`;
     } else {
-      return (
-        <>
-          {(bytes / gigaBytes).toFixed(2)} {decimalDisplay ? "GB" : "GiB"}
-        </>
-      );
+      return `${(bytes / gigaBytes).toFixed(2)} ${decimalDisplay ? "GB" : "GiB"}`;
     }
   }
   return "";
